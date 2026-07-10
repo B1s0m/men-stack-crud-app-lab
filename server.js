b2f06ef6edb3c4d84bb104cca0ebb0ef91801077
B1s0m <b0b.30916@gmail.com>
2026-07-10 18:24:34 +0300

    add  update funaction only add veiw update i do it by using createBlog.ejs

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express") //importing express package
 const app = express() // creates a express application
-const connectToDatabasef=require("./db")
+const connectToDatabasef = require("./db")
 const mongoose = require("mongoose");
-const Blog=require('./models/blog')
-const DataBlog=require("./models/dataBlogs")
+const Blog = require('./models/blog')
+const DataBlog = require("./models/dataBlogs");
+const { render } = require("ejs");
 app.use(express.static('public')) // serves all static files from public folder
 
 app.use(express.static('public')); //all static files are in the public folder
@@ -13,61 +14,99 @@ app.use(express.urlencoded({ extended: false }));
 connectToDatabasef.connectToDatabase()
 
 
- // this for add data in database
+// this for add data in database
 // DataBlog.sendData()
 
 
 
-app.get('/', async(req,res)=>{
+app.get('/', async (req, res) => {
     try {
-        
-        BlogFound=await Blog.find()
-        // console.log(BlogFound)
-        res.render('homepage.ejs',{BlogFound})
-
+        BlogFound = await Blog.find()
+        res.render('homepage.ejs', { BlogFound })
     } catch (error) {
-        console.log("error in found Blogs"+error)
+        console.log("error in found Blogs" + error)
     }
 })
 
 
-app.get('/createBlog',(req,res)=>{
-    res.render('createBlog.ejs') 
+app.get('/createBlog', (req, res) => {
+    res.render('createBlog.ejs',{ Blog: null})
 })
 
-app.get('/:id', async(req,res)=>{
+
+app.get('/:id', async (req, res) => {
     try {
-        console.log(req.params.id)
-
-        oneBlogFound=await Blog.findById(req.params.id)
-        console.log(oneBlogFound)
-        res.render('datilesBlog.ejs',{Blog:oneBlogFound})  
-        
-
+        oneBlogFound = await Blog.findById(req.params.id)
+        res.render('datilesBlog.ejs', { Blog: oneBlogFound })
     } catch (error) {
-        console.log("error in found Blogs"+error)
+        console.log("error in found Blogs" + error)
     }
 })
 
 
 
-app.post('/blogs', async (req,res)=>{
+app.post('/:id/delete', async (req, res) => {
 
-    try{
-    console.log(req.body)
-    req.body.published=Boolean(req.body.published)
-    console.log( req.body.published)
+    try {
+        const deleteBlog = await Blog.findByIdAndDelete(req.params.id)
+        res.redirect('/')
+    }
+    catch (err) {
+        console.log("Delete Problem : " + err)
+    }
 
-    const createdBlog = await Blog.create(req.body)
-    console.log(createdBlog._id)
-    res.redirect('/')
+
+})
+
+app.get('/:id/update', async (req, res) => {
+
+
+    console.log(req.params.id);
+    oneBlogFound = await Blog.findById(req.params.id)
+    res.render('createBlog.ejs', { Blog: oneBlogFound })         
+    
+
+
+})
+
+
+
+
+app.post('/blogs', async (req, res) => {
+
+    try {
+        // console.log(req.body)
+        req.body.published = Boolean(req.body.published)
+        // console.log( req.body.published)
+
+        const createdBlog = await Blog.create(req.body)
+        // console.log(createdBlog._id)
+        res.redirect('/')
 
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
-    
-   
+
+
+})
+app.post('/blogs/update/:id', async (req, res) => {
+
+    try {
+        // console.log(req.body)
+        req.body.published = Boolean(req.body.published)
+        // console.log( req.body.published)
+         console.log(req.body);
+        // const createdBlog = await Blog.create(req.body)
+        // console.log(createdBlog._id)
+        res.redirect('/')
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+
 })
 
 
@@ -79,6 +118,6 @@ app.post('/blogs', async (req,res)=>{
 
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('App is Running')
 }) // listen on port 3000
